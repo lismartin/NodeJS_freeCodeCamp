29cc06ad6f214d729d0f3dc1afdf2ec34b0ab297
lismartin <ldcmartiny@gmail.com>
2022-06-27 04:59:54 +0000

    updating on the tutorial

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,8 +1,23 @@
-const http = require('http');
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-	res.write('Welcome to my server!');
-	res.end();
-});
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000);
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores importante info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3,  [4]]]]
+
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
+console.log("hello world!!");
